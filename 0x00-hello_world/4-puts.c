76ac101ffe1748cf66119bf76418667b2e0cd1d8
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-16 16:23:32 -0800

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * Printing ( "Programming is like building a multilingual puzzle ) using puts function
+ * return value = 0
+ */ 
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * Printing ( with proper grammar, but the outcome is a piece of art, ) using printf function
+ * return value = 0
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
