88d2ace29d88cc99cc1bf606ecb5a8dab9c82391
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-17 16:31:53 -0800

    Task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible combinations of
+ * single-digit numbers using putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putcahr(',');
+			putchar(' ');
+		}
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
