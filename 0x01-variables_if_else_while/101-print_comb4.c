c82d970cbe1d0e3e312e86e97d2b9d2d5e9c9000
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-17 17:08:04 -0800

    Task-11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible different combinations
+ * of three digits (the three digits must be different) using
+ * putcahr function six times
+ * Return: 0
+ */
+int main(void)
+{
+        int i, j, k;
+
+        for (i = 0; i < 8; i++)
+        {
+                for (j = i + 1; j < 9; j++)
+                {
+			for (k = j + 1; k < 10; k++)
+			{
+                        	putchar(i + '0');
+                        	putchar(j + '0');
+				putchar(k + '0');
+                        	if (i != 7)
+                        	{
+                        	        putchar(',');
+                        	        putchar(' ');
+                        	}
+                }
+        }
+
+        putchar('\n');
+
+        return (0);
+}
+
