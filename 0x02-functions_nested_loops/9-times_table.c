d37d7d0f2a0cd414358303ac35a4174809fc929b
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-21 14:36:58 -0800

    Task-9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - Print the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int i, j, x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			x = i * j;
+			
+			if (j == 0)
+				_putchar('0');
+			else if (x > 9)
+			{
+				_putchar(' ');
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(x + '0');
+			}
+			if (j == 9)
+				_putchar('\n');
+			else
+				_putchar(',');
+		}
+	}
+}
