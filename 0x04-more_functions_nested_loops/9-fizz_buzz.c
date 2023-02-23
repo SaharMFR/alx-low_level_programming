c749259bf4251ad595555051a1f44dbaaf6679bc
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-23 11:08:22 -0800

    Task-9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz test.
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%i ", i);
+	}
+
+	return (0);
+}
