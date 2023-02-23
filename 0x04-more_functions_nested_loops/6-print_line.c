314977bb9d16a0793d39ae6f6fb96b8c6e2ea09c
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-23 08:42:07 -0800

    Task-6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line by printing character '_'
+ * n times followed by a new line , using _putchar function.
+ * @n: the number of times (how long the straight line is) (int type).
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+}
