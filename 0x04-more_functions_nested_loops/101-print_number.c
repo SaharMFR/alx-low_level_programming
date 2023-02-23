3381e2ef35b092c53d9011839d861d19111dd42a
Sahar Mohamed <sweetysahora@gmail.com>
2023-02-23 14:05:21 -0800

    Task-12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_number - Print an integer using _putchar function.
+ * @n: the number to print (int type).
+ */
+void print_number(int n)
+{
+	long power = 1, longN;
+	int check = 1;
+
+	longN = n;
+	/* if negative */
+	if (longN < 0)
+	{
+		_putchar('-');
+		longN *= -1;
+	}
+
+	while (ckeck)
+	{
+		if ((longN / (power * 10)) > 0)
+			power *= 10;
+		else
+			check = 0;
+	}
+
+	while (longN >= 0)
+	{
+		if (power == 1)
+		{
+			_putchar((longN % 10) + '0');
+			longN = -1;
+		}
+		else
+		{
+			_putchar(((longN / power) % 10) + '0');
+			power /= 10;
+		}
+	}
+}
