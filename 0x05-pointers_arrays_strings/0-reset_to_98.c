06add7af9d2d5f03e821e47264a44813dfcaf77c
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-02-28 00:44:20 +0200

    Task-0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Take a pointer to an integer as a parameter
+ * and update the value it points to to 98.
+ * @n: the integer (type pointer).
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
