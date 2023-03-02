541926185a67aafc4228bf283519676e821185d2
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-03 01:59:09 +0200

    update on task-1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,10 +13,9 @@ char *_strcat(char *dest, char*src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; src[j] != '\0' && n > 0; n--, i++; j++)
 	{
 		dest[i] = src[j];
-		i++;
 	}
 
 	dest[i] = '\0';
