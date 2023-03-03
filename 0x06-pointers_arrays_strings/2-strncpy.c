e28afe8601846e34d66c3c5047e1fe9a5fb656b1
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-03 22:49:53 +0200

    update2 in task-2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * @dest: the string we add in (char type string).
  * @src: the string we add from (char type string).
  * @n: number of characters that we add (type int).
- * Retrun: pointer to the modified string 'dest'.
+ * Return: pointer to the modified string 'dest'.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
