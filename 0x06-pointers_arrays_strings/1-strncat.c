9858b8b1fe2993f2b90a915c3f1a613149a7bb69
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-03 19:25:53 +0200

    update2 in task-1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _strcat - Concatenate two strings.
+ * _strncat - Concatenate two strings.
  * @dest: the string we put the concatenated string.
  * @src: the string we add to the first one.
  * @n: the long from the second one (type int).
  * Return: pointer to the resulting string 'dest'.
  */
-char *_strcat(char *dest, char*src, int n)
+char *_strncat(char *dest, char*src, int n)
 {
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0' && n > 0; n--, i++; j++)
+	for (j = 0; (src[j] != '\0') && (n > 0); n--, i++; j++)
 	{
 		dest[i] = src[j];
 	}
