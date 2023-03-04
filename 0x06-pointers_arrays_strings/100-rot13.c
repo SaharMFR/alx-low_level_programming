e5ef24cc4502234cedb7a212b5c1760f349fc318
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-04 05:06:52 +0200

    update5 in task-8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,11 +11,9 @@ char *rot13(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if ((str[i] >= 65 && str[i] <= 90 && (str[i] + 13) > 90) || (str[i] >= 97 && str[i] <= 122 && (str[i] + 13) > 122)
-		{
-			str[i] = (str[i] - 13);
-		}
+			str[i] -= 13;
 		else if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
-			str[i] = (str[i] + 13);
+			str[i] += 13;
 	}
 
 	return (str);
