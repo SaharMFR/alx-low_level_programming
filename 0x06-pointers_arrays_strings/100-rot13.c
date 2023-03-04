50233d5c194624c9fa60f22e257bfdf8cf48eabd
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-04 04:14:51 +0200

    Task-8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rot13 - Encode a atring using rot13.
+ * @str: the string (char type string).
+ * Return: encoded string.
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
+		{
+			str[i] = (str[i] <= 90 && str[i] + 13 > 90) ?
+				(str[i] - 13) : (str[i] + 13);
+		}
+	}
+
+	return (str);
+}
