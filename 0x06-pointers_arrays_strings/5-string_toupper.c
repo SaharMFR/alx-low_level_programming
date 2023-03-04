b7c742a23ef50dc83289a747dc452895ea333079
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-04 02:03:28 +0200

    Task-5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - Change all lowercase letters of astring
+ * to uppercase.
+ * @str: the string (char type string).
+ * Return: the uppercase string.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+
+	return (str);
+}
