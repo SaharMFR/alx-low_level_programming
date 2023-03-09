b9e356f80c91e77f49dc16c25264903139e9956a
Sahar Mohamed <sweetysahora@gmail.com>
2023-03-09 10:51:02 -0800

    Task-5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - Find the natural square root
+ * of a number (using recursion).
+ * @n: the number (type int).
+ * Return: the natural square root if it exists,
+ * '-1' if it does not exist (type int).
+ */
+int _sqrt_recursion(int n)
+{
+	return (assistantFunction(n, 1));
+}
+
+/**
+ * assistantFunction - Assistant function to the function
+ * _sqrt_recursion.
+ * @n: the number (type int).
+ * @sqrt: the square root (type int).
+ * Return: the natural square root, '-1' if it does not
+ * exist.
+ */
+int assistantFunction(int n, int sqrt)
+{
+	if ((sqrt * sqrt) == n)
+		return (sqrt);
+	else if (sqrt > (n / 2))
+		return (-1);
+
+	return (assistantFunction(n, sqrt + 1));
+}
