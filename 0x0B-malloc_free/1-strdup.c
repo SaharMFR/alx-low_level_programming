227bb6ed27960d2bdb5f85d49c9a0926c52d2c95
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-15 23:26:06 +0200

    Task-1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strdup - Create a newly allocated space in memory
+ * which contains a copy of the string given.
+ * @str: the string given (char type string).
+ * Return : pointer to the new string, NULL if str = NULL
+ * of if insufficient memory was available.
+ */
+cahr *_strdup(char *str)
+{
+	char *arr;
+	int i, j;
+
+	if (i = 0; str[i] != '\0'; i++)
+		;
+
+	arr = malloc((sizeof(*arr) * i) + 1)
+	if (arr == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		arr[j] = str[j];
+	arr[j] = '\0';
+
+	return (arr);
+}
