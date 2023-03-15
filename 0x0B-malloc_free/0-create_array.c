861a8d33ee41261775607a0267ba4ee5ddf016dc
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-15 22:54:48 +0200

    Task-0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - Create an array of chars , and
+ * initialize it with a specific char .
+ * @size: the size of the array (type unsigned int).
+ * @c: the char (type char).
+ * Return: NULL if size = 0 or if it fails,
+ * pointer to the array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(*arr) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
