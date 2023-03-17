78c94905ce385c7056e10584f40e6d2e14f81156
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-18 01:58:54 +0200

    update2 in task-1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,20 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - find length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
 /**
  * string_nconcat - Concatenate 2 strings.
  * @s1: the first string (char type string).
@@ -11,23 +27,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newString;
-	int length1, i, j;
+	int strlen1, i, c;
 
-	for (length1 = 0; s1[length1] != '\0'; length1++)
-		;
- 
-	newString = malloc(sizeof(char) * (length1 + n + 1));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	for (i = 0; i < length1; i++)
-		newString[i] = s1[i];
-
-	for (j = 0; j < n && s2[j] != '\0'; j++)
+	strlen1 = (unsigned int)_strlen(s1);
+	newString = malloc((strlen1 + n + 1) * sizeof(char));
+	if (newString == NULL)
+		return (NULL);
+	for (i = 0, c = 0; i < (strlen1 + n); i++)
 	{
-		newString[i] = s2[j];
-		i++;
+		if (i < strlen1)
+			newString[i] = s1[i];
+		else
+			newString[i] = s2[c++];
 	}
-
 	newString[i] = '\0';
 
-	return (newString);
+	return (p);
 }
+
