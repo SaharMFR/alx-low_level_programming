edd05daf93d35e22de40899688b8033a606736ff
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-18 02:12:30 +0200

    update4 in task-1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * _strlen - find length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
-}
+#include <stdlib.h>
 
 /**
  * string_nconcat - Concatenate 2 strings.
@@ -27,26 +14,38 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newString;
-	int strlen1, i, c;
+	unsigned int i, j, k, l;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	strlen1 = (unsigned int)_strlen(s1);
-	newString = malloc((strlen1 + n + 1) * sizeof(char));
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
+	if (n < j)
+		j = n;
+
+	j += i;
+	newString = malloc(sizeof(char *) * (j + 1));
+
 	if (newString == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
-	{
-		if (i < strlen1)
-			newString[i] = s1[i];
-		else
-			newString[i] = s2[c++];
-	}
-	newString[i] = '\0';
 
+	for (k = 0; k < i; k++)
+		newString[k] = s1[k];
+	for (l = 0; l < j; l++)
+	{
+		newString[k] = s2[l];
+		k++;
+	}
+	k++;
+	newString[k] = '\0';
 	return (newString);
 }
-
