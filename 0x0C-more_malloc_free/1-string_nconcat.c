d56ddaf7c482a09b99e76d52b318b31fab955aac
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-18 04:19:09 +0200

    update5 in task-1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newString;
-	unsigned int i, j, k, l;
+	unsigned int i, j, k, l, length;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,30 +22,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
+		;
 
 	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+		;
 
 	if (n < j)
 		j = n;
 
-	j += i;
-	newString = malloc(sizeof(char *) * (j + 1));
+	length = i + n;
+	newString = malloc(length + 1);
 
 	if (newString == NULL)
 		return (NULL);
 
-	for (k = 0; k < i; k++)
-		newString[k] = s1[k];
-	for (l = 0; l < j; l++)
+	for (k = 0; k < length; k++)
 	{
-		newString[k] = s2[l];
-		k++;
+		if (k < i)
+			newString[k] = s1[k];
+		else
+			newString[k] = s2[k - i];
+
 	}
-	k++;
+
 	newString[k] = '\0';
+
 	return (newString);
 }
