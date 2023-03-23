60140f7f6c272f8e48a4ce4f4a2dcf63a063f955
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-23 22:41:53 +0200

    update2 in tasks 1,2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  * array_iterator - Execute a function given as a parameter 
- * 	on each element of an array.
+ * on each element of an array.
  * @array: array of integers.
  * @size: the size of the array.
  * @action: pointer to the function we need to use (type int).
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,10 +4,10 @@
  * @array: array of integers.
  * @size: the number of elements in the array.
  * @cmp: pointer to the function to be used to
- * 	compare values.
+ * compare values.
  * Return: the index of the first element for
- * 	which the 'cmp' funciotn doesn't return 0,
- * 	-1 if no element matches of size <= 0.
+ * which the 'cmp' funciotn doesn't return 0,
+ * -1 if no element matches of size <= 0.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
