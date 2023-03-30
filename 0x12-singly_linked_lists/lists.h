30eb56b85cae564517a0d53568b0c6c1013642ba
Sahar Mohamed <saharmohamed@Sahars-MacBook-Air.local>
2023-03-30 22:19:43 +0200

    update in struct in task-0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,18 +4,18 @@
 #include <stdlib.h>
 
 /**
- * struct list_t - Singly linked list.
+ * struct list_s - Singly linked list.
  * @s: the string.
  * @len: the length of the string.
  * @next: pointer to the next node in the list.
  */
 
-typedef struct list_t
+typedef struct list_s
 {
 	char *s;
 	unisgned int len;
-	struct list_t *next;
-};
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 
