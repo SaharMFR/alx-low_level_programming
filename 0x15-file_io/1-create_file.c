02ebcd45c647d0d9b01b3701b3a91e7975f58561
Sahar Mohamed <100488384+SaharMFR@users.noreply.github.com>
2023-07-10 21:15:50 +0300

    update in task-1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
-		return (0);
+		return (-1);
 
 	if (text_content)
 	{
