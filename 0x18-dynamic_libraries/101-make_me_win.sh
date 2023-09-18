58ea57e7405f1ff576cda5e6db33c43a8d6245aa
SaharMFR <sweetysahora@gmail.com>
2023-09-18 15:56:40 -0700

    update1 in task-3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/SaharMFR/alx-low_level_programming/tree/master/0x18-dynamic_libraries/myhack.so
+wget -P /tmp/ https://raw.github.com/SaharMFR/alx-low_level_programming/master/0x18-dynamic_libraries/myhack.so
 export LD_PRELOAD=/tmp/myhack.so
